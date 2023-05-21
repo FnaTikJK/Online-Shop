f3ee91e10d26f2aa2176f79908091ff55ef36f1b
Anton Yaskov <yaskovanton333@gmail.com>
2023-05-21 14:32:26 +0500

    FavoritePage some fixes

--- front/src/Pages/Favorite/FavoritePage.tsx
+++ front/src/Pages/Favorite/FavoritePage.tsx
@@ -2,37 +2,48 @@ import React, {useEffect, useState} from 'react';
 import {ProductShortDTO} from "../../APIHelper/Requesters/ProductRequester";
 import {FavoriteRequester} from "../../APIHelper/Requesters/FavoriteRequester";
 import ProductCardComp from "../../GeneralComponents/ProductCard/ProductCardComp";
+import "./FavoritePage.css"
+import {NavigateFunction, useNavigate} from "react-router-dom";
+
+async function GetFavoritesAsync(navigate: NavigateFunction): Promise<ProductShortDTO[] | undefined>{
+    try {
+        let response = await FavoriteRequester.GetAllFavoritesAsync()
+        return response.data;
+    }
+    catch (e: any){ // AxiosError
+        if (e.response.status === 401)
+            navigate("/Auth");
+        alert(e);
+    }
+}
+
+function DrawProducts(products: ProductShortDTO[]): JSX.Element[] {
+    return products.map((p) =>
+        <ProductCardComp product={p} initIsFavorited={true} initCount={0} />);
+}
 
 const FavoritePage = () => {
     const [products, setProducts] = useState<ProductShortDTO[] | null>(null);
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        GetFavoritesAsync();
-    }, [])
+    useEffect(() => {(async () => {
+        setProducts(await (GetFavoritesAsync(navigate)) ?? []);
+    })()}, [])
+
+    if (products === null)
+        return (<>
+            <h1>Избранное</h1>
+            <>Загрузка...</>
+        </>);
 
     return (
         <>
             <h1>Избранное</h1>
-            {products === null ?
-                <>
-                    Loading...
-                </>
-                :
-                    products.map((p) =>
-                <ProductCardComp product={p} initIsFavorited={true} initCount={0}/>
-            )}
+            {DrawProducts(products)}
         </>
     );
 
-    async function GetFavoritesAsync(){
-        try {
-            let response = await FavoriteRequester.GetAllFavoritesAsync()
-            setProducts(response.data);
-        }
-        catch (e){ // AxiosError
-            alert(e);
-        }
-    }
+
 };
 
 export default FavoritePage;

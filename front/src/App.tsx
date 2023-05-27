82444ab2fcd2a777c595ac702cf6a05ea1e1d27f
Anton Yaskov <yaskovanton333@gmail.com>
2023-05-27 15:20:51 +0500

    Replace navigation into header, remove footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import RegisterAuthPage from "./Pages/RegisterAuth/RegisterAuthPage";
 import { Route, Routes} from "react-router-dom";
-import FooterComp from "./GeneralComponents/Footer/FooterComp";
 import HomePage from "./Pages/Home/HomePage";
 import HeaderComp from "./GeneralComponents/Header/HeaderComp";
 import BasketPage from "./Pages/Basket/BasketPage";
@@ -14,11 +13,9 @@ import ProductPage from "./Pages/Product/ProductPage";
 function App() {
     return (
     <Routes>
-        <Route path={"/"} element={<FooterComp />}>
-            <Route path={"/"} element={<HeaderComp />}>
-                <Route index element={<HomePage/>}/>
-                <Route path={"/Products/:id"} element={<ProductPage />}/>
-            </Route>
+        <Route path={"/"} element={<HeaderComp />}>
+            <Route index element={<HomePage/>}/>
+            <Route path={"/Products/:id"} element={<ProductPage />}/>
             <Route path={"Basket"} element={<BasketPage />}/>
             <Route path={"Favorites"} element={<FavoritePage />}/>
             <Route path={"Profile"} element={<ProfileOptionsListPage />}/>
--- front/src/GeneralComponents/Header/HeaderComp.tsx
+++ front/src/GeneralComponents/Header/HeaderComp.tsx
@@ -1,21 +1,38 @@
 import React, {useState} from 'react';
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import {Button} from "antd";
 import Search from "antd/lib/input/Search";
+import styles from "./HeaderComp.module.css";
+import {HeartOutlined, ShoppingCartOutlined, UserOutlined} from "@ant-design/icons";
 
 const HeaderComp = () => {
     const [pageState, setPageState] = useState<"Simple" | "Categories">("Simple");
+    const navigate = useNavigate();
 
     if (pageState === "Simple")
         return (
             <>
-                <Search
-                    placeholder={"Поиск"}
-                    allowClear
-                    enterButton={"Поиск"}
-                    size={"large"}
-                    suffix={<Button onClick={() => setPageState("Categories")}>Cat</Button>}
-                />
+                <div className={styles.Mock}/>
+                <header className={styles.Header}>
+                    <div className={styles.Logo}>Logo</div>
+                    <div className={styles.Search}>
+                        <Search
+                            placeholder={"Поиск"}
+                            allowClear
+                            enterButton={"Поиск"}
+                            size={"large"}
+                            suffix={<Button onClick={() => setPageState("Categories")}>Cat</Button>}
+                        />
+                    </div>
+
+                    <div className={styles.Navigation}>
+                        <UserOutlined style={{ fontSize: '25px' }} onClick={() => {navigate("Profile")}}/>
+                        <HeartOutlined
+                            style={{  fontSize: '25px', marginLeft:"10px" }} onClick={() => {navigate("Favorites")}}/>
+                        <ShoppingCartOutlined
+                            style={{ fontSize: '25px', marginLeft:"10px" }} onClick={() => {navigate("Basket")}}/>
+                    </div>
+                </header>
 
                 <Outlet />
             </>

91f24c3e84620fbab22a7a5f4f4f3400c8899203
Anton Yaskov <yaskovanton333@gmail.com>
2023-05-29 16:25:32 +0500

    Added FiltersComp

---
+++ front/src/Pages/Home/FiltersComp/FiltersComp.tsx
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import {useSearchParams} from "react-router-dom";
+import {Button} from 'antd';
+import styles from "./FiltersComp.module.css";
+import SliderComp from "./SliderComp/SliderComp";
+import CategoriesComp from "./CategoriesComp/CategoriesComp";
+import {FiltersDTO, SearchRequester} from "../../../APIHelper/Requesters/SearchRequester";
+import SortingComp from "./SortingComp/SortingComp";
+
+let filterParams: {[key: string]: string | string[]} = {};
+
+async function GetFiltersAsync(): Promise<FiltersDTO | undefined>{
+    try {
+        let response = await SearchRequester.GetFiltersAsync();
+        return response.data
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+function AddParams(key: string, value: string | string[]){
+    filterParams[key] = value;
+}
+
+function ApplyParams(searchParams: URLSearchParams, setSearchParams: any){
+    for(let key in filterParams) {
+        if (typeof filterParams[key] === "string") {
+            if (filterParams[key] === "")
+                searchParams.delete(key);
+            else
+                searchParams.set(key, filterParams[key] as string);
+        }
+        else {
+            let arr = filterParams[key] as string[];
+            if (arr.length == 0)
+                searchParams.delete(key);
+            else
+                arr.forEach(e => searchParams.append(key, e));
+        }
+    }
+    setSearchParams(searchParams);
+}
+
+function ResetParams(searchParams: URLSearchParams, setSearchParams: any){
+    for(let key in filterParams)
+        searchParams.delete(key);
+    setSearchParams(searchParams);
+}
+
+const FiltersComp = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [filters, setFilters] = useState<FiltersDTO | undefined>(undefined);
+
+    useEffect(() => {(async () => {
+        setFilters(await GetFiltersAsync());
+    })()}, [])
+
+    if (!filters)
+        return (<div>Загрузка...</div>);
+
+    return (
+        <div className={styles.DivMain}>
+            <SliderComp initFrom={filters.from} initTo={filters.to} addParams={AddParams}/>
+            <SortingComp addParams={AddParams}/>
+            <CategoriesComp categories={filters.categories} addParams={AddParams}/>
+            <Button onClick={() => ApplyParams(searchParams, setSearchParams)}>Применить</Button>
+            <Button onClick={() => ResetParams(searchParams, setSearchParams)}>Сбросить</Button>
+        </div>
+    );
+};
+
+export default FiltersComp;
